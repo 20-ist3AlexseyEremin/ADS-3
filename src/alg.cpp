9fd78a2e51c6cf342247d78a40e096d374ca68bf
20-ist3AlexseyEremin <78901825+20-ist3AlexseyEremin@users.noreply.github.com>
2021-04-25 03:23:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,73 +3,73 @@
 #include "tstack.h"
 
 
-int priorit(char sym) {
-        if ((sym == '+') || (sym == '-'))
-                return 2;
-        else if ((sym == '*') || (sym == '/'))
-                return 3;
-        else
-                return 0;
-        }
+int priority(char sym) {
+	if (sym == '(')
+		return 0;
+	else if (sym == ')')
+		return 1;
+	else if ((sym == '+') || (sym == '-'))
+		return 2;
+	else
+		return 3;
+}
 
 std::string infx2pstfx(std::string inf) {
-                std::stack<char> stack;
-                std::string str;
-        for (int i = 0; i < inf.length(); i++) {
-        if ((inf[i] >= '0') && (inf[i] <= '9')) {
-                str = str + inf[i];
-                str = str + " ";
-        } else if ((stack.empty()) || (inf[i] == '(') || (priorit(inf[i] > priorit(stack.top())))) {
-                stack.push(inf[i]);
-        } else if (inf[i] == ')') {
-                while (stack.top() != '(') {
-                str = str + stack.top();
-                str = str + ' ';
-                stack.pop();
+        TStack<char> stack;
+        std::string res;
+        for (int i = 0; i < inf.length(); ++i) {
+                if (isdigit(inf[i])) {
+                res += inf[i];
+                res += ' ';
         }
-                stack.pop();
-        } else {
-                while (!stack.empty() && (priorit(stack.top()) >= priorit(inf[i]))) {
-                str = str + stack.top();
-                str = str + ' ';
-                stack.pop();
-        }
+        else if ((inf[i] == '(') || (stack.isEmpty()) || (priority(inf[i]) > priority(stack.get()))) {
                 stack.push(inf[i]);
         }
-        }
-        if (!stack.empty()) {
-                while (!stack.empty()) {
-                str = str + stack.top();
-                str = str + ' ';
+        else if (inf[i] == ')') {
+                char x = stack.get();
+                stack.pop();
+        while (x != '(') {
+                res += x;
+                res += ' ';
+                x = stack.get();
                 stack.pop();
         }
         }
-        str.pop_back();
-        return str;
+        else {
+                while (!stack.isEmpty() && (priority(stack.get()) >= priority(inf[i]))) {
+        res += stack.get();
+        res += ' ';
+        stack.pop();
+        }
+        stack.push(inf[i]);
+        }
+        }
+                while (!stack.isEmpty()) {
+                res += stack.get();
+                res += ' ';
+                stack.pop();
+        }
+        res.pop_back();
+        return res;
         }
 
 int eval(std::string pst) {
-        std::stack<char> stack;
-        int result;
+        TStack<int> stack;
         for (int i = 0; i < pst.length(); i++) {
-        if ((pst[i] >= '0') && (pst[i] <= '9')) {
-                 stack.push(pst[i] - '0');
-        } else if (pst[i] != ' ') {
-                 int num2 = stack.top();
-                 stack.pop();
-                 int num1 = stack.top();
-                 stack.pop();
-      if (pst[i] == '+')
-                 stack.push(num1 + num2);
-      if (pst[i] == '-')
-                 stack.push(num1 - num2);
-      if (pst[i] == '*')
-                 stack.push(num1 * num2);
-      if (pst[i] == '/')
-                 stack.push(num1 / num2);
-      }
-      }
-      result = stack.top();
-      return result;
-      return 0;
-      }
+                if (isdigit(pst[i])) {
+        stack.push( pst[i] - '0' );
+        }
+        else if (pst[i] != ' ') {
+                int num2 = stack.get();
+                stack.pop();
+                int num1 = stack.get();
+                stack.pop();
+        if (pst[i] == '*') stack.push(num1 * num2);
+        else if (pst[i] == '/') stack.push(num1 / num2);
+        else if (pst[i] == '+') stack.push(num1 + num2);
+        else if (pst[i] == '-') stack.push(num1 - num2);
+        }
+        }
+                return stack.get();
+        }
+      
